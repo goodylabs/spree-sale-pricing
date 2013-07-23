43e7f8d54502427137e07b30e1947262c90409b4
Wojciech Baran <wojciech.baran@goodylabs.com>
2013-07-23 14:43:07 +0200

    Fixed the bug where condition (was broken on mysql)

--- app/models/spree/sale_price.rb
+++ app/models/spree/sale_price.rb
@@ -11,7 +11,7 @@ module Spree
     attr_accessible :value, :start_at, :end_at, :enabled
 
     scope :active, lambda {
-      where("enabled = 't' AND (start_at <= ? OR start_at IS NULL) AND (end_at >= ? OR end_at IS NULL)", Time.now, Time.now)
+      where("enabled = ? AND (start_at <= ? OR start_at IS NULL) AND (end_at >= ? OR end_at IS NULL)", true, Time.now, Time.now)
     }
 
     attr_accessible :value, :start_at, :end_at, :enabled
